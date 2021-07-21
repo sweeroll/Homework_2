b400afb0c62812d0c8d9cf66e28a1c778f7ddfa2
Максим Родионов <sweeroll.prg@gmail.com>
2021-07-21 17:28:17 +0300

    Solution

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
-# Тут будет Ваше исполнение ДЗ
+from typing import List
 
-def calculate(my_input):
+
+def calculate(grid: List[List]) -> int:
     """Заменить эту функцию своей."""
+    if not grid:
+        return 0
+
+    def f(y: int, x: int) -> None:
+        """Поиск соседних единиц."""
+        grid[y][x] = 0
+        if x > 0 and grid[y][x - 1] == 1:
+            f(y, x - 1)
+        if x < len(grid[0]) - 1 and grid[y][x + 1] == 1:
+            f(y, x + 1)
+        if y > 0 and grid[y - 1][x] == 1:
+            f(y - 1, x)
+        if y < len(grid) - 1 and grid[y + 1][x] == 1:
+            f(y + 1, x)
+
+    number = 0
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            if grid[y][x] == 1:
+                number += 1
+                f(y, x)
+    return number
